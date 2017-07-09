48c6bfeb86bb5266ecbcd046aa2124a3bad7fa1d
Jose Olortegui <jolortegui98@gmail.com>
2017-07-08 20:38:00 -0400

    Login form basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,7 +11,7 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
     AppRoutingModule
   ],
   providers: [],
