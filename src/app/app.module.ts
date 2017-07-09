91196397ae5df5322fe3693a803ecbf3fac0d091
Jose Olortegui <jolortegui98@gmail.com>
2017-07-09 10:07:32 -0400

    Login with logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'dashboard',  component: LoginComponent },
+  { path: 'dashboard',  component: DashboardComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
