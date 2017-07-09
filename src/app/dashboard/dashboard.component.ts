5ac61fc6fac60195bf5ef2efab3dc0758830b2d4
rodrigo valdez <rodri.valdez@gmail.com>
2017-07-09 12:33:36 -0400

    servicio de lineas de accion

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LineaAccionService } from '../linea-accion.service'; 
+import { LineaAccion } from '../linea-accion'; 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
  //template: 'asfd',
-  styleUrls: ['./dashboard.component.sass']
+  styleUrls: ['./dashboard.component.sass'],
+    providers:[LineaAccionService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  lineasAccion: LineaAccion[];
+ 
 
-  ngOnInit() {
+  constructor(private lineaAccionService: LineaAccionService) { }
+
+  getLineasAccion(): void {
+
+  	console.log()
+    this.lineaAccionService.getLineasAccion().then(lineasAccion => this.lineasAccion = lineasAccion);
+  }
+
+  ngOnInit(): void {
+    this.getLineasAccion();
   }
 
 }
